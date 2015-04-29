481e3924297785837bd9ee6a592f0edf5693dfeb
Thiago Lagden <lagden@gmail.com>
2015-04-29 16:26:24 -0300

    fix EOF and added static html

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ import Game  from './src/game';
 let game = new Game(40, 20);
 
 setInterval(() => {
-    console.log(game.render());
+    if (typeof window !== 'undefined') {
+        console.log(game.render());
+    } else {
+        process.stdout.write("\u001b[2J\u001b[0;0H");
+        process.stdout.write(game.render());
+    }
     game.step();
 }, 100);
